b16c70e79c1379516cad1dff8dddc88b7eaf720a
Diego Sousa <diegointeriores11@gmail.com>
2024-01-26 09:00:08 -0300

    refactor: adjust permission of workflow

--- action-entrypoint.sh
+++ action-entrypoint.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # Logando a variável de ambiente
+echo -e "INICIANDO EXECUÇÂO"
 echo -e "\n ADDED_FILES: $ADDED_FILES"
 echo -e "\n CHANGED_AND_MODIFIED_FILES: $CHANGED_AND_MODIFIED_FILES"
 echo -e "\n GITHUB_ACTION_PATH: $GITHUB_ACTION_PATH"
