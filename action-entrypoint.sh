9fa9986ed33d5983096e9aebb95d9da386614278
Diego Sousa <diegointeriores11@gmail.com>
2024-01-26 15:20:49 -0300

    refactor: undo to specific function for table

--- action-entrypoint.sh
+++ action-entrypoint.sh
@@ -6,20 +6,18 @@ black="\e[30m"
 erros=""
 IFS=' ' read -ra files_array <<< "$CHANGED_AND_MODIFIED_FILES"
 
-function validatePattern() {
+function validateTableName() {
     local current_file=$1
-    local capture_target_pattern=$2
-    local forbidden_pattern=$3
-    local error_msg=$4
+    local capture_target_pattern=".*CREATE\s+TABLE\s+(IF\s+NOT\s+EXISTS\s+)?(\S+).*"
     local found_target=$(grep -i -E "$capture_target_pattern" "$current_file" | sed -E "s/$capture_target_pattern/\2/I")
     
-    if [[ "$found_target" =~ $forbidden_pattern ]]; then
-        erros+=$'\n'"\n$error_msg [$current_file]: \n$found_target"
+    if [[ "$found_target" =~ $TABLE_FORBIDDEN_PATTERN ]]; then
+        erros+=$'\n'"\nNomes de tabela no arquivo [$current_file]: \n$found_target"
     fi
 }
 
 for current_file in "${files_array[@]}"; do
-    validatePattern "$current_file" "CREATE\s+TABLE\s+(IF\s+NOT\s+EXISTS\s+)?(\S+)" $TABLE_FORBIDDEN_PATTERN "Nomes de tabela no arquivo"
+    validateTableName $current_file
 done
 
 if [ -n "$erros" ]; then
