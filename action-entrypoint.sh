27ff79822f10891ddf4302548318dc04e00b93dd
Diego Sousa <diegointeriores11@gmail.com>
2024-01-26 17:13:51 -0300

    refactor: adjust sh to capture multiple lines

--- action-entrypoint.sh
+++ action-entrypoint.sh
@@ -8,12 +8,16 @@ IFS=' ' read -ra files_array <<< "$CHANGED_AND_MODIFIED_FILES"
 
 function validateTableName() {
     local current_file=$1
-    local capture_target_pattern=".*CREATE\s+TABLE\s+(IF\s+NOT\s+EXISTS\s+)?(\S+).*"
-    local found_target=$(grep -i -E "$capture_target_pattern" "$current_file" | sed -E "s/$capture_target_pattern/\2/I")
-    
-    if [[ "$found_target" =~ $TABLE_FORBIDDEN_PATTERN ]]; then
-        erros+=$'\n'"\nNomes de tabela no arquivo [$current_file]: \n$found_target"
-    fi
+    local found_create_table_groups=$(awk '/create[[:space:]]+table(\s+if\s+not\s+exists)?/,/;/ {printf "%s", $0} /;/ {print ""}' "$current_file")
+
+    while IFS= read -r group; do
+        local capture_target_pattern=".*CREATE\s+TABLE\s+(IF\s+NOT\s+EXISTS\s+)?(\S+).*"
+        local captured_table_name=$(echo "$group" | grep -i -E "$capture_target_pattern" | sed -E "s/$capture_target_pattern/\2/I")
+        
+        if [[ "$captured_table_name" =~ $TABLE_FORBIDDEN_PATTERN ]]; then
+            erros+=$'\n'"-Nome de tabela no arquivo [$current_file]: \n  $captured_table_name"
+        fi
+    done <<< "$found_create_table_groups"
 }
 
 for current_file in "${files_array[@]}"; do
