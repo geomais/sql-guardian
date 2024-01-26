71529199eb1bfd312489cee7813775a14d26f2cd
Diego Sousa <diegointeriores11@gmail.com>
2024-01-26 13:37:50 -0300

    refactor: adjust action to correctly validate tables

--- action-entrypoint.sh
+++ action-entrypoint.sh
@@ -6,21 +6,29 @@ echo -e "CHANGED_AND_MODIFIED_FILES: $CHANGED_AND_MODIFIED_FILES\n"
 echo -e "TABLE_FORBIDDEN_PATTERN: $TABLE_FORBIDDEN_PATTERN\n"
 echo -e "COLUMN_FORBIDDEN_PATTERN: $COLUMN_FORBIDDEN_PATTERN\n"
 
-colecao_valores=("$ADDED_FILES" "$CHANGED_AND_MODIFIED_FILES")
+red='\033[41m'
+green='\033[42m'
+nc='\033[0m'
+all_changed_files=("$ADDED_FILES" "$CHANGED_AND_MODIFIED_FILES")
 erros=""
 
-for valor in "${colecao_valores[@]}"; do
-    padrao="CREATE\s+TABLE\s+$TABLE_FORBIDDEN_PATTERN"
+function validateTableNames() {
+    local current_file=$1
+    local table_pattern=".*CREATE\s+TABLE\s+(.*)"
+    local table_name=$(grep -i -E "$table_pattern" "$current_file" | sed -E "s/$table_pattern/\1/I")
     
-    if grep -q -i -E "$padrao" "$valor"; then
-        erros+="Erro no arquivo $valor: Padrão '$padrao' encontrado"$'\n'
+    if [[ "$table_name" =~ $TABLE_FORBIDDEN_PATTERN ]]; then
+        erros+=$'\n'"\nArquivo: $current_file: \n$table_name"
     fi
+}
 
+for current_file in "${all_changed_files[@]}"; do
+    validateTableNames "$current_file"
 done
 
 if [ -n "$erros" ]; then
-    echo -e "Erros encontrados:\n$erros"
+    echo -e "${red}Erros encontrados:$erros${nc}"
     exit 1
 else
-    echo -e "Nenhum erro encontrado. Execução concluída com sucesso."
+    echo -e "${green}Nenhum erro encontrado. Execução concluída com sucesso.${nc}"
 fi
