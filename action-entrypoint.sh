6d6a48b144dd9c43289c1082e9995c01df8adc66
Diego Sousa <diegointeriores11@gmail.com>
2024-01-26 17:15:00 -0300

    refactor: terminal formatting lines

--- action-entrypoint.sh
+++ action-entrypoint.sh
@@ -4,7 +4,6 @@ red='\033[41m'
 green='\033[42m'
 black="\e[30m"
 erros=""
-IFS=' ' read -ra files_array <<< "$CHANGED_AND_MODIFIED_FILES"
 
 function validateTableName() {
     local current_file=$1
@@ -15,11 +14,12 @@ function validateTableName() {
         local captured_table_name=$(echo "$group" | grep -i -E "$capture_target_pattern" | sed -E "s/$capture_target_pattern/\2/I")
         
         if [[ "$captured_table_name" =~ $TABLE_FORBIDDEN_PATTERN ]]; then
-            erros+=$'\n'"-Nome de tabela no arquivo [$current_file]: \n  $captured_table_name"
+            erros+=$'\n'"- Nome de tabela no arquivo [$current_file]:\n  $captured_table_name"
         fi
     done <<< "$found_create_table_groups"
 }
 
+IFS=' ' read -ra files_array <<< "$CHANGED_AND_MODIFIED_FILES"
 for current_file in "${files_array[@]}"; do
     validateTableName $current_file
 done
