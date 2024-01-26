7c6af6f4a840f5f7f41a28764e8c25aee3b984a3
Diego Sousa <diegointeriores11@gmail.com>
2024-01-26 13:50:54 -0300

    refactor; adjust color

--- action-entrypoint.sh
+++ action-entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo -e "INICIANDO EXECUÇÃO\n"
-echo -e "ALL_CHANGED_AND_MODIFIED_FILES: $ALL_CHANGED_AND_MODIFIED_FILES\n"
+echo -e "CHANGED_AND_MODIFIED_FILES: $CHANGED_AND_MODIFIED_FILES\n"
 echo -e "TABLE_FORBIDDEN_PATTERN: $TABLE_FORBIDDEN_PATTERN\n"
 echo -e "COLUMN_FORBIDDEN_PATTERN: $COLUMN_FORBIDDEN_PATTERN\n"
 
 red='\033[41m'
 green='\033[42m'
 nc='\033[0m'
+black="\e[30m"
 erros=""
 
 function validateTableNames() {
@@ -16,17 +17,17 @@ function validateTableNames() {
     local table_name=$(grep -i -E "$table_pattern" "$current_file" | sed -E "s/$table_pattern/\1/I")
     
     if [[ "$table_name" =~ $TABLE_FORBIDDEN_PATTERN ]]; then
-        erros+=$'\n'"\nArquivo: $current_file: \n$table_name"
+        erros+=$'\n'"\n${red}${black}Arquivo: $current_file: \n$table_name"
     fi
 }
 
-for current_file in "${$ALL_CHANGED_AND_MODIFIED_FILES[@]}"; do
+for current_file in "${CHANGED_AND_MODIFIED_FILES[@]}"; do
     validateTableNames "$current_file"
 done
 
 if [ -n "$erros" ]; then
-    echo -e "${red}Erros encontrados:$erros${nc}"
+    echo -e "${red}${black}Erros encontrados:$erros${nc}"
     exit 1
 else
-    echo -e "${green}Nenhum erro encontrado. Execução concluída com sucesso.${nc}"
+    echo -e "${green}${black}Nenhum erro encontrado. Execução concluída com sucesso.${nc}"
 fi
